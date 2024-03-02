16e2bbdbb3e5dd0942f97388265927dbca5b652f
FForzano <f.forzano99@gmail.com>
2024-03-02 17:15:03 +0100

    Update theme colors and logo in App component, and fix favicon and touch icon paths

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,28 @@ function App() {
       specUrl="https://api.formandopercorsi.com/doc/openapi.json"
       options={{
         nativeScrollbars: true,
-        theme: { colors: { primary: { main: '#dd5522' } } },
+        theme: {
+          colors: {
+            primary: {
+              main: '#D10C33'
+            }
+          },
+          sidebar: {
+            width: '260px',
+            backgroundColor: '#F3E8E8'
+          },
+          logo: {
+          },
+          typography: {
+            fontSize: '16px',
+            lineHeight: '1.4'
+          },
+          rightPanel: {
+          },
+        },
+        hideHostname: false,
+        hideDownloadButton: true,
+        hideLoading: true,
       }}
     />
   );
